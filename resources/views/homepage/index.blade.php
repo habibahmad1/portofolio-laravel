530ca5f8bfad89745d01c410fc75f2259e8ab102
habibahmad1 <oregairu2597@gmail.com>
2024-06-18 00:10:24 +0700

    done

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -18,8 +18,7 @@
         <section>
             <h1 class="titleIntro">About Me</h1>
             <h3>Web Developer | UI UX Design</h3>
-            <p>Welcome to my portfolio website, let me introduce myself, my name is Habib Ahmad, a bachelor's degree majoring in Information Systems. I have an interest in the world of programming, especially Web Development and Web Design, so if you need or have work with Web Development or Web Design, don't hesitate to contact me.
-            </p>
+            <p>Welcome to my portfolio website. My name is Habib Ahmad, and I hold a bachelor's degree in Information Systems. I am passionate about programming, particularly in Web Development and Web Design. If you have any projects or need assistance in these areas, please feel free to reach out to me.</p>
             <a href="">Hello</a>
         </section>
         <section>
@@ -29,18 +28,21 @@
 
     <div class="contact" id="contact">
         <h1 class="titleIntro">Contact Me</h1>
-        <div class="fullname">
-            <label for="#fullname">Fullname <span style="color: #fa1872">*</span></label>
-            <input type="text" id="fullname" placeholder="Your Name">
-        </div>
-        <div class="email">
-            <label for="#email">Email <span style="color: #fa1872">*</span></label>
-            <input type="email" id="email" placeholder="Your Email">
-        </div>
-        <div class="message">
-            <label for="#message">Message <span style="color: #fa1872">*</span></label>
-            <textarea name="message" id="message" placeholder="Write Messages"></textarea>
-        </div>
-        <button type="submit" id="sendmessage">Send Message</button>
+        <form action="">
+
+            <div class="fullname">
+                <label for="#fullname">Fullname <span style="color: #fa1872">*</span></label>
+                <input type="text" id="fullname" placeholder="Your Name" required>
+            </div>
+            <div class="email">
+                <label for="#email">Email <span style="color: #fa1872">*</span></label>
+                <input type="email" id="email" placeholder="Your Email" required>
+            </div>
+            <div class="message">
+                <label for="#message">Message <span style="color: #fa1872">*</span></label>
+                <textarea name="message" id="message" placeholder="Write Messages" required></textarea>
+            </div>
+            <button type="submit" id="sendmessage">Send Message</button>
+        </form>
     </div>
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,8 +1,8 @@
 <div class="container-sidebar">
     <ul>
         <a href="#home"><li><i class="fa-solid fa-house"></i> Home</li></a>
-        <a href=""><li><i class="fa-solid fa-user"></i> About</li></a>
-        <a href=""><li><i class="fa-solid fa-address-book"></i> Contact</li></a>
+        <a href="#about"><li><i class="fa-solid fa-user"></i> About</li></a>
+        <a href="#contact"><li><i class="fa-solid fa-address-book"></i> Contact</li></a>
     </ul>
 </div>
 
@@ -10,7 +10,7 @@
     <div class="container-navbar">
         <div class="tes">
             <img src="https://cdn.pixabay.com/photo/2013/07/13/10/12/sketchbook-156775_640.png" alt="logo" width="20px">
-            <a href="/" id="titleNav">Habib<span style="color: #ff780a"> Ahmad</span></a>
+            <a href="#home" id="titleNav">Habib<span style="color: #ff780a"> Ahmad</span></a>
         </div>
         <ul>
             <a href="#home"><li>Home</li></a>
